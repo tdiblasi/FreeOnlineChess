157722eecb72925be74e3f452dd84e138d16868a
Thomas DiBlasi <tdiblasi111@gmail.com>
2023-06-15 23:41:26 -0400

    new architecture

---
+++ api/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors')
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./firebase_admin_config.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://two-player-chess-6342c-default-rtdb.firebaseio.com/'
+});
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(cors());
+
+var db = admin.database();
+var ref = db.ref("github");
+ref.on("value", (snapshot) => {
+  console.log(snapshot.val());
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
